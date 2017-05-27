e19ba1d707d5176dcaf2f7cb604742c05e59c93f
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-05-27 11:17:22 -0400

    Checked Nav.js out of the webpack2 branch

---
+++ src/Nav/Nav.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import styles from './styles.module.css';
+
+export default class Nav extends React.Component {
+    render() {
+
+        let loggedInLinks = (<div>
+            <Link className={styles.headerItem} to="/viewer"><span>Viewer</span></Link>
+            <Link className={styles.headerItem} to="/datasets"><span>Datasets</span></Link>
+            <Link to='/logout'><span>Logout</span></Link>
+                             </div>);
+        let loggedOutLinks = (<div>
+            <Link className={styles.headerItem} to="/viewer"><span>Viewer</span></Link>
+            <Link to="/datasets" className={styles.headerItem}><span>Datasets</span></Link>
+            <Link to='/login'><span>Login</span></Link>
+                             </div>);
+
+
+        console.log('logged in', this.props.auth.loggedIn())
+        let linksToDisplay = this.props.auth.loggedIn() ? loggedInLinks : loggedOutLinks;
+
+            return (
+          <div className={styles.navBar} >
+            {"Navbar"}
+            <div>
+                <Link to="/home">HiGlass</Link>
+            </div>
+            <div>
+                {linksToDisplay}
+            </div>
+          </div>
+        );
+    }
+}
